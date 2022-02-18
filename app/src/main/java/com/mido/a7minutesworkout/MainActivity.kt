15253820a8936e4b17eec730f4993c31fead89e3
Mohamed <mohamedalaa4j@gmail.com>
2022-02-18 18:33:10 +0200

    viewBinding implemented

--- app/src/main/java/com/mido/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/mido/a7minutesworkout/MainActivity.kt
@@ -3,15 +3,25 @@ package com.mido.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import com.mido.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        flStart.setOnClickListener {
+        binding?.flStart?.setOnClickListener {
             Toast.makeText(this, "hi", Toast.LENGTH_LONG).show()
         }
+    }
+
+    // clean way to un-assign binding to avoid memory leak
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
