091202f033477de7069d1b3c7bdcead9d25c6538
Mohamed <mohamedalaa4j@gmail.com>
2022-02-18 18:11:47 +0200

    139 done

--- app/src/main/java/com/mido/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/mido/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,16 @@ package com.mido.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        flStart.setOnClickListener {
+            Toast.makeText(this, "hi", Toast.LENGTH_LONG).show()
+        }
     }
 }
