2f83be5053186ef0e63d0ca7ea3da4c477909d9e
Mohamed <mohamedalaa4j@gmail.com>
2022-02-20 14:56:13 +0200

    144 rest timer done

--- app/src/main/java/com/mido/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/mido/a7minutesworkout/ExerciseActivity.kt
@@ -2,10 +2,18 @@ package com.mido.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.mido.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private var binding: ActivityExerciseBinding? = null
+
+    ///// Resting & relaxing timer
+    private var restTimer: CountDownTimer? = null
+
+    ///// Rest time for resting timer
+    private var  restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +29,53 @@ class ExerciseActivity : AppCompatActivity() {
         ///// The listener of back button in the toolbar
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed() // A go back function
+        }
+
+        setupRestView()
+
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private fun setRestProgressBar(){
+        binding?.progressBar?.progress = restProgress
+
+        restTimer = object : CountDownTimer(10000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress ).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Here now we will start the exercise.", Toast.LENGTH_SHORT).show()
+            }
+
+        }.start()
+    }
+
+    ///// Check & cancel RestTimer if it's not null in case back button pressed -> and call setRestProgressBar()
+    private fun setupRestView(){
+
+        ///// Reset timer on destroying the activity
+        if (restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        ///// A clean way to un-assign binding to avoid memory leak
+        binding = null
+
+        ///// Reset timer on destroying the activity
+        if (restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
         }
     }
 }
